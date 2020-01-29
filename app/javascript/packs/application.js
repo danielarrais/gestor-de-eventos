9ee589f2d004e8f63a6e6f13cfa74337d6140e4b
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-01-28 23:06:35 -0300

    Configura o Argon Design System, Bootstrap e o Jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,14 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-import "@fortawesome/fontawesome-free/js/all";
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
+
+//Bootstrap 4
+import 'bootstrap'
+import '../stylesheets/application'
+
+// Icones
+import '@fortawesome/fontawesome-free/js/all';
+
+// Argon design
+import 'argon-design-system-free/assets/js/argon.min'
