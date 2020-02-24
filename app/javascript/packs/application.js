57376c4ff564aad7649b4bba2cc58deb15096fd7
danielarrais <danielarraiscarvalho@gmail.com>
2020-02-24 12:05:04 -0300

    Adiciona  o CSS e o JS do ARGON ao webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,12 @@ require('@rails/activestorage').start();
 require('channels');
 
 //Bootstrap 4
-import 'bootstrap'
+import '../js/argon-design-system'
 import '../stylesheets/application'
 
 // Icones
 import '@fortawesome/fontawesome-free/js/all';
 
-// Argon design
-import 'argon-design-system-free/assets/js/argon.min'
 
 // Mensagens de alerta
 global.toastr = require('toastr');
