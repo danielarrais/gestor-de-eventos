4d473ebcde713595f485ce0049e17c3c78cc88c6
danielarrais <danielarraiscarvalho@gmail.com>
2020-02-25 18:59:09 -0300

    Importa os arquivos de estilo e js nos arquivos principais do webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,23 @@ require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
 
+import 'jquery'
+import '../js/core/popper.min.js'
+import '../js/core/bootstrap.min.js'
+import '../js/plugins/perfect-scrollbar.jquery.min.js'
+import '../js/plugins/bootstrap-switch.js'
+import '../js/plugins/nouislider.min.js'
+import '../js/plugins/glide.js'
+import '../js/plugins/choices.min.js'
+import '../js/plugins/bootstrap-switch.js'
+import '../js/plugins/datetimepicker.js'
+import '../js/plugins/headroom.min.js'
+import '../js/argon-design-system.js?v=1.0.0'
+
 //Bootstrap 4
-import '../js/argon-design-system'
 import '../stylesheets/application'
+
+
 
 // Icones
 import '@fortawesome/fontawesome-free/js/all';
