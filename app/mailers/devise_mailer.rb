4a4227265d2378eb25c9d48324c4c1d88291051f
danielarrais <danielarraiscarvalho@gmail.com>
2020-02-26 23:13:55 -0300

    Substitui classe de envios de emails do devise

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,10 @@
+class DeviseMailer < Devise::Mailer
+  helper :application
+  include Devise::Controllers::UrlHelpers
+  default template_path: 'users/mailer'
+
+  def create_password_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :create_password_instructions, opts)
+  end
+end
