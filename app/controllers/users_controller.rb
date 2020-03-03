7dc9beab258cffb2daa27ed1f1e82545ca8e7e6f
danielarrais <danielarraiscarvalho@gmail.com>
2020-03-02 22:26:16 -0300

    Adiciona input de perfis no formulário de usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_profiles_for_select, only: [:edit, :new]
 
   # GET /users
   # GET /users.json
@@ -62,13 +63,23 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :surname, :registration, :cpf, :email, :cellphone, :date_of_birth, :encrypted_password)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_profiles_for_select
+    @profiles = Profile.all.select(:name, :id).map{| k, v| [k.name, k.id] }
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name,
+                                 :surname,
+                                 :registration,
+                                 :cpf, :email,
+                                 :cellphone, :date_of_birth,
+                                 :encrypted_password, :profile_ids => [])
+  end
 end
