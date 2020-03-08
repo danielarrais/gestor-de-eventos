ed114a902a9cf48854c3b47e48be97258b0aa80c
danielarrais <danielarraiscarvalho@gmail.com>
2020-03-08 12:58:04 -0300

    Configura o dominio do sistema

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
+  config.hosts << "danielarrais.dev"
   config.consider_all_requests_local = true
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
