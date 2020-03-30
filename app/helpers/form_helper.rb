ced46a84bf67bccc09d530ea18496dc67a0f00e0
danielarrais <danielarraiscarvalho@gmail.com>
2020-03-29 22:32:02 -0300

    Cria Helper para criação de URL de imagem FAKE

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -44,6 +44,11 @@ module FormHelper
     end
   end
 
+  # Retornar uma URL de imagem fake com base nos paramêtros
+  def url_image_fake(resolution:, text: nil, image_color: '0a0c0d', text_color: 'fff', text_size: '15')
+    "https://fakeimg.pl/#{resolution}/#{image_color},100/#{text_color},255?retina=1&font_size=#{text_size}&text=#{text ||= resolution}"
+  end
+
   # Cria label para colocar traduções alternativas
   def label(*sources)
     if sources[0].to_s.include? '['
