446645c5b966d88375341c15f2a416438575f0e2
danielarrais <danielarraiscarvalho@gmail.com>
2020-04-12 21:46:14 -0300

    Adiciona relações a tabela de eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  belongs_to :image
+  belongs_to :event_category
 end
--- db/migrate/20200412230859_create_events.rb
+++ db/migrate/20200412230859_create_events.rb
@@ -4,11 +4,14 @@ class CreateEvents < ActiveRecord::Migration[6.0]
       t.string :name, null: false
       t.timestamp :start_date, null: false
       t.timestamp :closing_date, null: false
-      t.bigint :category_id, null: false
+      t.bigint :event_category_id, null: false
       t.bigint :image_id, null: false
       t.integer :workload, null: false
 
       t.timestamps
     end
+
+    add_foreign_key :events, :images
+    add_foreign_key :events, :event_categories
   end
 end
