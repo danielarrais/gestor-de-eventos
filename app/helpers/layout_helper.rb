fa0726ab8153faba54048b327dd47fdc35ef6b44
danielarrais <danielarraiscarvalho@gmail.com>
2020-04-13 22:20:37 -0300

    Cria helper para título de página

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,2 +1,13 @@
 module LayoutHelper
+  def page_title(title)
+    content_tag('div', class: 'ct-page-title') do
+      concat content_tag('h1', title, class: 'ct-title')
+    end.html_safe
+  end
+
+  def icon_link(text, icon, path)
+    content_tag('div', class: 'float-right pt-4') do
+      concat(link_to("<span><i class='fa fa-#{icon}'></i> #{text}</span>".html_safe, path))
+    end
+  end
 end
