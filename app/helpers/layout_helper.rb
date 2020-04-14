dd99b6c5659924cd579e616ae57f02fd5c7c0060
danielarrais <danielarraiscarvalho@gmail.com>
2020-04-13 23:05:50 -0300

    Ajusta helpers criados

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,13 +1,20 @@
 module LayoutHelper
   def page_title(title)
-    content_tag('div', class: 'ct-page-title') do
-      concat content_tag('h1', title, class: 'ct-title')
-    end.html_safe
+    content_tag'div', class: 'ct-page-title' do
+      concat content_tag'h1', title, class: 'ct-title'
+    end
   end
 
   def icon_link(text, icon, path)
-    content_tag('div', class: 'float-right pt-4') do
-      concat(link_to("<span><i class='fa fa-#{icon}'></i> #{text}</span>".html_safe, path))
+    span_icon = content_tag 'span' do
+      concat icon icon, 1
+      concat text
     end
+
+    link_to span_icon.html_safe, path
+  end
+
+  def icon(icon, margin = 0)
+    content_tag('i', class: "fa fa-#{icon} mr-#{margin}") do end
   end
 end
