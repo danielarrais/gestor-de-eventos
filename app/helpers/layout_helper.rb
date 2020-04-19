c3b37dc833e2b9fa5a5f1feffea17a19d18bcc54
danielarrais <danielarraiscarvalho@gmail.com>
2020-04-18 21:50:30 -0300

    Cria helper para visualização de imagens

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,7 +1,39 @@
 module LayoutHelper
+  def view_image_button(url, title, button_text: '', id: Util::genarate_random_string(25))
+    url = url ||= url_image_fake(text: 'IMAGEM NÃO ENCONTRADA', resolution: '500x500')
+
+    @content = link_to "<span><i class='fa fa-search #{'mr-1' unless button_text.blank?}'></i>#{button_text}</span>".html_safe, '#',
+                       class: 'btn btn-default btn-icon btn-1 btn-simple', data: { toggle: "modal", target: "##{id}" }
+
+    @content << content_tag(:div, id: id, class: 'modal fade',
+                            tabindex: '-1', role: 'dialog',
+                            'aria-labelledby' => "#{id}_label",
+                            'aria-hidden' => "true") do
+      content_tag :div, class: 'modal-dialog modal-dialog-centered modal-lg', role: 'document' do
+        content_tag :div, class: 'modal-content' do
+          concat(content_tag(:div, class: 'modal-header') do
+            concat(content_tag(:h5, class: 'modal-title', id: "#{id}_label") do
+              title
+            end)
+            concat(button_tag('', { class: "close", 'data-dismiss' => "modal", 'aria-label' => "Close" }) do
+              content_tag(:span, 'aria-hidden' => 'true') do
+                '&times;'.html_safe
+              end
+            end)
+          end)
+          concat(content_tag(:div, class: 'modal-body') do
+            content_tag(:div, class: 'text-center') do
+              image_tag url, style: 'max-height: 500px; max-width: 750px;', id: "#{id}-image"
+            end
+          end)
+        end
+      end
+    end
+  end
+
   def page_title(title)
-    content_tag'div', class: 'ct-page-title' do
-      concat content_tag'h1', title, class: 'ct-title'
+    content_tag 'div', class: 'ct-page-title' do
+      content_tag 'h1', title, class: 'ct-title'
     end
   end
 
@@ -14,7 +46,8 @@ module LayoutHelper
     link_to span_icon.html_safe, path
   end
 
-  def icon(icon, type: :fa,  margin: 0)
-    content_tag('i', class: "#{type} #{type}-#{icon} mr-#{margin}") do end
+  def icon(icon, type: :fa, margin: 0)
+    content_tag('i', class: "#{type} #{type}-#{icon} mr-#{margin}") do
+    end
   end
 end
