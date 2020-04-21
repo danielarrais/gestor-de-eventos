19f3dc5ab89abf99472e5e77e32929a5bc97c488
danielarrais <danielarraiscarvalho@gmail.com>
2020-04-21 12:53:33 -0300

    Carrega todos os choices ao carregar a página

--- app/javascript/js/custom/inputs-conf.js
+++ app/javascript/js/custom/inputs-conf.js
@@ -1,10 +1,12 @@
 import flatpickr from 'flatpickr'
 import {Portuguese} from 'flatpickr/dist/l10n/pt.js';
+import Choices from "choices.js";
 
 // Cria definições de máscaras
 jQuery(function($){
    $('.cpf-mask').mask('000.000.000-00', {reverse: true});
    $('.cellphone-mask').mask('(00) 0000-0000');
+   loadingChoices();
 });
 
 // Define as configurações padrões dos data pickers
@@ -12,3 +14,12 @@ flatpickr.setDefaults({
    'locale': Portuguese,
    'dateFormat': 'd/m/Y'
 });
+
+// Cria choices
+function loadingChoices() {
+   $('select.choices').each((i, element) => {
+      new Choices(element, {
+         removeItemButton: true,
+      });
+   })
+}
