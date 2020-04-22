ee211539c26d9d507278874f60873e554f961ead
danielarrais <danielarraiscarvalho@gmail.com>
2020-04-21 22:38:05 -0300

    Cria vinculo de atividades orientadas com orientandos no model

--- app/models/oriented_activity.rb
+++ app/models/oriented_activity.rb
@@ -2,5 +2,8 @@ class OrientedActivity < ApplicationRecord
   alias_attribute :advisors, :people
 
   has_and_belongs_to_many :people # use alias guiding
+  has_and_belongs_to_many :guideds
+
   accepts_nested_attributes_for :people
+  accepts_nested_attributes_for :guideds
 end
