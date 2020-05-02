c95121e75e114ef7b09b8c9f5af0f76511ad2e0f
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-05-02 16:32:58 -0300

    Restaura migração excluída

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,17 +42,36 @@ namespace :puma do
   after :start, :nginx_restart
 end
 
-# namespace :git do
-#   desc 'Runs any rake task, cap deploy:rake task=db:rollback'
-#   task seed_db: [:set_rails_env] do
-#     on release_roles([:db]) do
-#       within release_path do
-#         with rails_env: fetch(:rails_env) do
-#           execute :rake, 'db:seed'
-#         end
-#       end
-#     end
-#   end
-#   # before :clone, :seed_db
-# end
+namespace :deploy do
+  desc 'Runs any rake task, cap deploy:rake task=db:rollback'
+  task seed: [:set_rails_env] do
+    on release_roles([:db]) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  task create: [:set_rails_env] do
+    on release_roles([:db]) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          begin
+            ActiveRecord::Base.connection
+          rescue
+            p 'creating database'
+            execute :rake, 'db:create'
+          else
+            p 'database already created'
+            exit 0
+          end
+        end
+      end
+    end
+  end
+  before :compile_assets, :create
+  after :migrate, :seed
+end
 
