a69517629dbf111ac9363f9c28a7f68d98271a56
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-06-08 22:06:54 -0300

    Creates rake to register situation keys

--- app/models/event_request.rb
+++ app/models/event_request.rb
@@ -1,8 +1,25 @@
 class EventRequest < ApplicationRecord
+  after_create :create_initial_situation
+
   belongs_to :event
   belongs_to :person, required: false
   has_one :situation, -> { order('created_at desc').limit(1) }, as: :origin
   has_many :situations, -> { order('created_at asc') }, as: :origin
 
   accepts_nested_attributes_for :event, allow_destroy: false
+
+  def create_initial_situation
+    self.situations.create(person: person,
+                           key_situation: KeySituation.find_by(key: :draft))
+  end
+
+  def forward
+    return if
+    self.situations.create(person: person,
+                           key_situation: KeySituation.find_by(key: :forwarded))
+  end
+
+  def forward?
+    situation&.key_situation&.key&.to_sym == :forwarded
+  end
 end
---
+++ lib/tasks/seeder/key_situations.rake
@@ -0,0 +1,11 @@
+namespace :seeder do
+  desc "Generates keys for dealing with situations"
+  task key_situations: :environment do
+    KeySituation.find_or_create_by(key: :draft)
+        .update(description: 'Rascunho', description_female: 'Rascunho')
+    KeySituation.find_or_create_by(key: :forwarded)
+        .update(description: 'Enviado', description_female: 'Enviada')
+    KeySituation.find_or_create_by(key: :returned_for_correction)
+        .update(description: 'Devolvido para correção', description_female: 'Devolvida para correção')
+  end
+end
