4aeec19c87661db413f5b546790e7de5512c23b3
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-09-13 00:29:55 -0300

    Adjust filter class to generate methods for boolean attributes

--- app/utils/filter.rb
+++ app/utils/filter.rb
@@ -7,11 +7,15 @@ class Filter
     attributes.each do |k, v|
       self.instance_variable_set("@#{k}", v)
       self.class.send(:define_method, k, proc { self.instance_variable_get("@#{k}") })
+      self.class.send(:define_method, "#{k}?", proc { self.instance_variable_get("@#{k}") }) if is_boolean?(v)
       self.class.send(:define_method, "#{k}=", proc { |v| self.instance_variable_set("@#{k}", v) })
     end
   end
 
   def to_h
+  end
 
+  def is_boolean?(value)
+    [true, false].include? value
   end
 end
